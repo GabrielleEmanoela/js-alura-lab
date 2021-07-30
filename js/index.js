18efca125cf9b7729182bc99d315d47231e87c8e
Gabrielle Oliveira <biela.a09@gmail.com>
2021-07-29 22:29:11 -0300

    Manipulações modulo #02 TextContent, Query selector.

--- js/index.js
+++ js/index.js
@@ -3,3 +3,24 @@ var title = document.querySelector("h1");
 console.log(title.textContent); //pegando apenas oq tem dentro.
 title.textContent = "Mudando o title";
 console.log(title.textContent);
+//------------------------------------//
+
+var paciente = document.querySelector("#primeiropaciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(paciente);
+console.log("====================================");
+console.log(tdPeso);
+console.log("====================================");
+console.log(peso);
+console.log("====================================");
+console.log(altura);
+console.log("====================================");
+
+var imc = (peso / altura) * altura;
+console.log("====================================");
+console.log(imc);
+console.log("====================================");
