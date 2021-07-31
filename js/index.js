11d20d96edc36af4133baea28148784698fd514a
Gabrielle Oliveira <biela.a09@gmail.com>
2021-07-31 18:08:08 -0300

    Adicionando td.

--- js/index.js
+++ js/index.js
@@ -54,5 +54,32 @@ function mensagemq() {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log("Fui clicado");
+  var form = document.querySelector("#add-form");
+  var pacienteTr = document.createElement("tr");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td"); //cria elemento td
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd); //adiciona elemento td na linha
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
 });
