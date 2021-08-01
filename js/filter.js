eacdc993331591d59f6d3a9e2c7ba5ace82cc9c7
Gabrielle Oliveira <biela.a09@gmail.com>
2021-08-01 02:02:06 -0300

    Adicionando expressão regular.

--- js/filter.js
+++ js/filter.js
@@ -6,7 +6,8 @@ filtro.addEventListener("input", function () {
       var paciente = pacientes[i];
       var tdNome = paciente.querySelector(".info-nome");
       var nome = tdNome.textContent;
-      if (nome != this.value) {
+      var expressao = new RegExp(this.value, "i");
+      if (!expressao.test(nome)) {
         paciente.classList.add("invisivel");
       } else {
         paciente.classList.remove("invisivel");
