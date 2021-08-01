6d68caa4279a583b8ddc9e8ae74687e42cec806d
Gabrielle Oliveira <biela.a09@gmail.com>
2021-08-01 01:52:41 -0300

    Melhorando o filtro.

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,21 @@
 var filtro = document.querySelector("#filtrar-tabela");
 filtro.addEventListener("input", function () {
   var pacientes = document.querySelectorAll(".paciente");
-  for (let i = 0; i < pacientes.length; i++) {
-    var paciente = pacientes[i];
-    var tdNome = paciente.querySelector(".info-nome");
-    var nome = paciente.textContent;
+  if (this.value.length > 0) {
+    for (let i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var tdNome = paciente.querySelector(".info-nome");
+      var nome = tdNome.textContent;
+      if (nome != this.value) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  } else {
+    for (let i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
+    }
   }
 });
